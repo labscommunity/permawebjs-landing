df4caf332f4f09f1387dafd73242ce823aa3b053
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-10-16 18:47:50 +0100

    fix: gh file path + npm var

--- .github/scripts/createFile.js
+++ .github/scripts/createFile.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 
-const JSON_FILE_PATH = path.join(__dirname, '../../src/components/plugIn/plugins.json');
-const TEMPLATE_PATH = path.join(__dirname, '../../src/components/plugIn/plugInTemplate.tsx');
-const OUTPUT_DIR = path.join(__dirname, '../../src/pages/plugins/');
-const PREVIOUS_STATE_PATH = path.join(__dirname, '../../src/components/plugIn/pluginState.json');
+const JSON_FILE_PATH = '../../src/components/plugIn/plugins.json'
+const TEMPLATE_PATH = '../../src/components/plugIn/plugInTemplate.tsx'
+const OUTPUT_DIR = '../../src/pages/plugins/'
+const PREVIOUS_STATE_PATH = '../../src/components/plugIn/pluginState.json'
 
 const currentData = JSON.parse(fs.readFileSync(JSON_FILE_PATH, 'utf8'));
 const previousData = JSON.parse(fs.readFileSync(PREVIOUS_STATE_PATH, 'utf8'));
@@ -15,7 +15,7 @@ const newEntries = currentData.filter(entry =>
 );
 
 newEntries.forEach(newEntry => {
-    let content = templateContent.replace('%%PACKAGENAME%%', newEntry.name).replace('%%NPMNAME%%', newEntry.name);
+    let content = templateContent.replace('%%PACKAGENAME%%', newEntry.name).replace('%%NPMNAME%%', newEntry.npmName);
     fs.writeFileSync(path.join(OUTPUT_DIR, `${newEntry.name}.tsx`), content, 'utf8');
     console.log(`Created new file for ${newEntry.name}`);
 });
